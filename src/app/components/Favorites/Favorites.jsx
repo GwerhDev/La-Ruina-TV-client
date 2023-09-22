187a208a3a4d8095416d6b5a88b9e3af33ac6bec
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-22 12:06:06 -0300

    favorites styles

--- src/app/components/Favorites/Favorites.jsx
+++ src/app/components/Favorites/Favorites.jsx
@@ -17,61 +17,64 @@ export const Favorites = () => {
   useEffect(() => {
     dispatch(getFavorites(userId))
   }, [dispatch, userId]);
-  
+
   return (
     <div>
       <div className={s.container}>
         <div className='navFixed'></div>
-          <div className={s.divContLikes}>
-            <div className={s.divHeader}>
-              <div className="header-container">
-                { favorites?.length
-                  ? <><h1>Tus favoritos</h1>
-                    <h3>Encuentra tu contenido favorito aquí</h3></>
-                  : <><h1>Nada por aquí...</h1>
-                    <h3>¡Agrega contenido a tus favoritos!</h3></>
-                }
-              </div>
+        <div className={s.divContLikes}>
+          <div className={s.divHeader}>
+            <div className="header-container">
+              {favorites?.length
+                ? <>
+                  <h1>Tus favoritos</h1>
+                  <h3>Encuentra tu contenido favorito aquí</h3>
+                </>
+                : <>
+                  <h1>Nada por aquí...</h1>
+                  <h3>¡Agrega contenido a tus favoritos!</h3>
+                </>
+              }
             </div>
             <ul className={s.contLikes}>
               {
-                favorites?.map((e,index)=>{
-                  return(
+                favorites?.map((e, index) => {
+                  return (
                     <li className={s.liLikes} key={index}>
-                      <Link to={`/view/v=${e.id}`}>
-                        <img 
-                        onClick={()=>{
-                          return (
-                            dispatch(resetOption()),
-                            $d(`.bodyApp`).style.transform='translateX(0)',
-                            $d(`.navCont`).style.transitionDuration='.2s',
-                            $d(`.bodyApp`).style.transitionDuration='2s',
-                            $d(`.navCont`).style.width='100vw',
-                            $d(`.browserBody`).style.height='auto',
-                            $d(`.browserBody`).style.overflowY='scroll',
-                            $d(`.visor`).style.transform='translateX(0)',
-                            $d('#slideCanvasCont').style.overflowY="scroll"
-                        )}}   
-                        className={s.likeImg}
-                        src={RenderDriveImage(e.imageSlider)} 
-                        alt='img' 
-                        height='100px' />
-                      <div className={s.divH3}>
-                        <h3 style={{marginTop: '0px', fontSize: '10px', marginBottom: '-4px'}}>
-                            {e.artist}
-                        </h3>
-                        <h3 style={{marginTop: '0px', fontSize: '17px'}}>
-                            {e.title}
-                        </h3>
-                      </div>
-                      </Link>
+                        <Link to={`/view/v=${e.id}`}>
+                          <div className={s.imageContainer} 
+                               style={{ backgroundImage: `url(${RenderDriveImage(e.imageSlider)})` }}
+                               onClick={() => {
+                                return (
+                                  dispatch(resetOption()),
+                                  $d(`.bodyApp`).style.transform = 'translateX(0)',
+                                  $d(`.navCont`).style.transitionDuration = '.2s',
+                                  $d(`.bodyApp`).style.transitionDuration = '2s',
+                                  $d(`.navCont`).style.width = '100vw',
+                                  $d(`.browserBody`).style.height = 'auto',
+                                  $d(`.browserBody`).style.overflowY = 'scroll',
+                                  $d(`.visor`).style.transform = 'translateX(0)',
+                                  $d('#slideCanvasCont').style.overflowY = "scroll"
+                                )
+                              }}>
+                            <div className={s.divH3}>
+                              <h3>
+                                {e.artist}
+                              </h3>
+                              <h2>
+                                {e.title}
+                              </h2>
+                            </div>
+                          </div>
+                        </Link>
                     </li>
                   )
                 })
               }
             </ul>
           </div>
-          <RequestProfile />
+        </div>
+        <RequestProfile />
       </div>
     </div>
   )
