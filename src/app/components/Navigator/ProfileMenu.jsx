69a10f73473bfd5f0586173ae7719f2e2fd29de7
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-03 16:37:57 -0300

    styling

--- src/app/components/Navigator/ProfileMenu.jsx
+++ src/app/components/Navigator/ProfileMenu.jsx
@@ -1,12 +1,9 @@
 import s from './ProfileMenu.module.css';
-
 import profileMenuCss from '../../../functions/ProfileMenu';
 import { OptionCanvas } from '../../../functions';
-
 import { getOption } from '../../../middlewares/redux/actions';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-
 import btnMenuTv from '../../../assets/images/ruinatv-icon-play-b.png';
 import likeIcon from '../../../assets/images/like-icon.png';
 import userIcon from '../../../assets/images/user-icon.png';
@@ -16,12 +13,12 @@ import configIcon from '../../../assets/images/config-icon.png';
 import subscriptionIcon from '../../../assets/images/subscription-icon.png';
 
 export const ProfileMenu = () => {
-  const currentUser = useSelector(state=>state.currentUser);
+  const currentUser = useSelector(state => state.currentUser);
   const dispatch = useDispatch();
   const history = useHistory();
   const { profilePic, username, role } = currentUser;
 
-  function onClickValue(e){
+  function onClickValue(e) {
     return (
       dispatch(getOption(e.target.value || e)),
       OptionCanvas(e.target.value || e)
@@ -30,100 +27,98 @@ export const ProfileMenu = () => {
 
   return (
     <div className={s.profileCont} id='profileCont'>
-        <ul className={s.profileBtnCont}>
-          <li 
-            className={s.profileBtnMenu} 
-            onClick={() => { return profileMenuCss('enter') }}
-            onMouseLeave={() => { return profileMenuCss('leave') }}
-            >
-              <span className={s.buttonProfile}>
-                <img className={s.userIcon} referrerPolicy="no-referrer" src={profilePic ? profilePic : userIcon} alt='userIcon' width='25px' />
-                  <p>Hola, <span>{username? username.substring(0, 5) + "..." : "Usuario"}</span></p>
-                <img className={s.btnMenuTv} src={btnMenuTv} alt='btnMenuTv' width='8px' />
-              </span>
+      <ul className={s.profileBtnCont}>
+        <li className={s.profileBtnMenu} onClick={() => profileMenuCss('enter')} onMouseLeave={() => profileMenuCss('leave')}>
+          <span className={s.buttonProfile}>
+            <img className={s.userIcon} referrerPolicy="no-referrer" src={profilePic ? profilePic : userIcon} alt='userIcon' width='25px'/>
+            <p>Hola, <span>{username ? username.substring(0, 5) + "..." : "Usuario"}</span></p>
+            <img className={s.btnMenuTv} src={btnMenuTv} alt='btnMenuTv' width='8px' />
+          </span>
+        </li>
+        <ul className={s.ulProfileOptions}>
+          <li className={s.liProfileMenuDisplay}>
+            <button
+              id='optionProfileBtn0'
+              className={s.optionProfileBtn}
+              value='profile'
+              onClick={(e) => { return onClickValue(e) }}
+              onMouseEnter={() => { return profileMenuCss('enter') }}>
+              <img
+                className={s.imgIconProf}
+                referrerPolicy="no-referrer"
+                src={profilePic ? profilePic : userIcon}
+                onClick={(e) => e.target.value = 'profile'}
+                alt=""
+              /> 
+              <br/>
+              PERFIL
+            </button>
           </li>
-          <ul className={s.ulProfileOptions}>
-            <li className={s.liProfileMenuDisplay}>
-              <button 
-                id='optionProfileBtn0' 
-                className={s.optionProfileBtn} 
-                value='profile' 
-                onClick={(e)=>{ return onClickValue(e) }}
-                onMouseEnter={() => { return profileMenuCss('enter') }}>
-                <img 
-                  className={s.imgIconProf}
-                  referrerPolicy="no-referrer" 
-                  src={profilePic ? profilePic : userIcon}
-                  onClick={(e) => { return e.target.value='profile' }}
-                  alt="" /> <br></br>
-                    PERFIL
-                </button>
-            </li>
-            <li className={s.liProfileMenuDisplay}>
-              <button 
-                id='optionProfileBtn2' 
-                className={s.optionProfileBtn} 
-                value='favorites' 
-                onClick={(e)=>{ return onClickValue(e) }}
-                onMouseEnter={() => { return profileMenuCss('enter') }}>
-              <img 
+          <li className={s.liProfileMenuDisplay}>
+            <button
+              id='optionProfileBtn2'
+              className={s.optionProfileBtn}
+              value='favorites'
+              onClick={(e) => { return onClickValue(e) }}
+              onMouseEnter={() => { return profileMenuCss('enter') }}>
+              <img
                 className={s.imgIcon}
-                onClick={(e) => { return e.target.value='favorites'} }
-                src={likeIcon} 
+                onClick={(e) => { return e.target.value = 'favorites' }}
+                src={likeIcon}
                 alt="" /><br></br>
-                MIS FAVORITOS
-              </button>
-            </li>
-            <li>
-              <button 
-                id='optionProfileBtn3' 
-                className={s.optionProfileBtn} 
-                value='config' 
-                onClick={(e)=>{return onClickValue(e)}}
-                onMouseEnter={() => { return profileMenuCss('enter') }}>
-              <img 
+              MIS FAVORITOS
+            </button>
+          </li>
+          <li>
+            <button
+              id='optionProfileBtn3'
+              className={s.optionProfileBtn}
+              value='config'
+              onClick={(e) => { return onClickValue(e) }}
+              onMouseEnter={() => { return profileMenuCss('enter') }}>
+              <img
                 className={s.imgIcon}
-                onClick={(e) => { return e.target.value='config' }}
-                src={configIcon} 
+                onClick={(e) => { return e.target.value = 'config' }}
+                src={configIcon}
                 alt="" /><br></br>
-                  CONFIGURACIÓN
-              </button>
-            </li>
-            <li>
-              <button                                 
-                id='optionProfileBtn5' 
-                className={s.optionProfileBtn} 
-                value={role==='admin'? 'dashboard' : 'subscription'}
-                onClick={(e)=>{ return onClickValue(e) }}
-                onMouseEnter={() => { return profileMenuCss('enter') }}>
-              <img 
+              CONFIGURACIÓN
+            </button>
+          </li>
+          <li>
+            <button
+              id='optionProfileBtn5'
+              className={s.optionProfileBtn}
+              value={role === 'admin' ? 'dashboard' : 'subscription'}
+              onClick={(e) => { return onClickValue(e) }}
+              onMouseEnter={() => { return profileMenuCss('enter') }}>
+              <img
                 className={s.imgIcon}
-                onClick={(e) => {return e.target.value=((role==='admin')? ('dashboard') : ('subscription'))}}
-                src={role==='admin'? adminIcon : subscriptionIcon} 
+                onClick={(e) => { return e.target.value = ((role === 'admin') ? ('dashboard') : ('subscription')) }}
+                src={role === 'admin' ? adminIcon : subscriptionIcon}
                 alt="" /><br></br>
-                {role==='admin'? 'DASHBOARD' : 'SUSCRIPCIÓN'}
-              </button>
-            </li>
-            <li className={s.ulSalirBtn}>
-              <button 
-                id='optionProfileBtn6' 
-                className={s.optionProfileBtn} 
-                onClick={()=>{
-                  return (
-                    localStorage.clear(),
-                    history.push('/browser'),
-                    window.location.reload()
-                  )
-                }}
-                onMouseEnter={() => { return profileMenuCss('enter') }}>
-              <img 
+              {role === 'admin' ? 'DASHBOARD' : 'SUSCRIPCIÓN'}
+            </button>
+          </li>
+          <li className={s.ulSalirBtn}>
+            <button
+              id='optionProfileBtn6'
+              className={s.optionProfileBtn}
+              onClick={() => {
+                return (
+                  localStorage.clear(),
+                  history.push('/browser'),
+                  window.location.reload()
+                )
+              }}
+              onMouseEnter={() => { return profileMenuCss('enter') }}>
+              <img
                 className={s.imgIcon}
-                src={logoutIcon} 
+                src={logoutIcon}
                 alt="" /><br></br>
-                  SALIR
-              </button>
-            </li>
-        </ul>   
+              SALIR
+            </button>
+          </li>
+        </ul>
       </ul>
     </div>
   )
