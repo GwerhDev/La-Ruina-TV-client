bce698310a0862fb8f9b72b7eb8e1ab4660a3fc4
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-04 18:16:28 -0300

    lateral menu

--- src/functions/Reset.js
+++ src/functions/Reset.js
@@ -4,7 +4,7 @@ export function reset() {
   $d(`.bodyApp`).style.transform = 'translateX(0)';
   $d(`.bodyApp`).style.transitionDuration = '2s';
   $d(`.navCont`).style.transitionDuration = '.2s';
-  $d(`.navCont`).style.width = '100%';
+  $d(`.navCont`).style.width = '100vw';
   $d(`.browserBody`).style.height = 'auto';
   $d(`.browserBody`).style.overflowY = 'scroll';
   $d(`.visor`).style.transform = 'translateX(0)';
