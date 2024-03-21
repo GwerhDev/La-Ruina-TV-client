07cb322aa5fed3e5706b519cf38d78539262088b
GwerhDev <gwerh.dev@gmail.com>
2024-03-21 12:09:40 -0300

    styling

--- src/functions/Reset.js
+++ src/functions/Reset.js
@@ -1,13 +1,6 @@
 import { $d } from "."
 
 export function reset() {
-  $d(`.bodyApp`).style.transform = 'translateX(0)';
-  $d(`.bodyApp`).style.transitionDuration = '2s';
-  $d(`.nav-container`).style.transitionDuration = '.2s';
-  $d(`.nav-container`).style.width = '100vw';
-  $d(`.browserBody`).style.height = 'auto';
-  $d(`.browserBody`).style.overflowY = 'scroll';
-  $d(`.visor`).style.transform = 'translateX(0)';
   $d('#slideCanvasCont').style.overflowY = 'hidden';
   $d('.canvas-menu-container').style.display = 'none';
 };
