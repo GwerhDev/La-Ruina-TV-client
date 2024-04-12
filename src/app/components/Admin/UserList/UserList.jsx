3f99311b7a7b9552e40afaba4bb9271c7d04619d
GwerhDev <gwerh.dev@gmail.com>
2024-04-12 12:41:55 -0400

    user list: updating profile pic or googlepic

--- src/app/components/Admin/UserList/UserList.jsx
+++ src/app/components/Admin/UserList/UserList.jsx
@@ -43,7 +43,7 @@ const EditUserList = () => {
               userList?.map((e, index) => {
                 return (
                   <ul className={s.list} key={index}>
-                    <li><img src={e.profilePic || "❗"} alt="Profile pic" height={"50px"} /></li> -
+                    <li><img src={e.googlePic || e.profilePic || "❗"} alt="Profile pic" height={"50px"} /></li> -
                     <li>{e.username || "❗"}</li> -
                     <li>{e.email || "❗"}</li> -
                     <li>{e.role || "❗"}</li> -
